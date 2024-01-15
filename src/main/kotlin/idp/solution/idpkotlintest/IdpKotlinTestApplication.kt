41d95d98aef425946f74c3821ba175280fd1b36e
조봉연 <by.cho@okestro.com>
2024-01-15 10:36:18 +0900

    initial commit

---
+++ src/main/kotlin/idp/solution/idpkotlintest/IdpKotlinTestApplication.kt
@@ -0,0 +1,11 @@
+package idp.solution.idpkotlintest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IdpKotlinTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<IdpKotlinTestApplication>(*args)
+}
